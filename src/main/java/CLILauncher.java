14a111603da129a106645fac92eb66a4d3d5e37e
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2016-12-01 13:48:46 +0100

    Fixed menu bar problem

--- src/main/java/CLILauncher.java
+++ src/main/java/CLILauncher.java
@@ -3,6 +3,7 @@ import com.rapidminer.tools.PlatformUtilities;
 
 import java.nio.file.Paths;
 
+// See: http://community.rapidminer.com/t5/RapidMiner-Studio/how-to-build-Rapidminer-Studio/m-p/33168
 public class CLILauncher
 {
     public static void main(String[] args) throws Exception
--- src/main/java/GUILauncher.java
+++ src/main/java/GUILauncher.java
@@ -4,11 +4,15 @@ import com.rapidminer.tools.PlatformUtilities;
 
 import java.nio.file.Paths;
 
+// See: http://community.rapidminer.com/t5/RapidMiner-Studio/how-to-build-Rapidminer-Studio/m-p/33168
 public class GUILauncher
 {
     public static void main(String[] args) throws Exception
     {
         System.setProperty(PlatformUtilities.PROPERTY_RAPIDMINER_HOME, Paths.get("").toAbsolutePath().toString());
+        // See: http://community.rapidminer.com/t5/Become-a-RapidMiner-Developer/Source-build-ClassCastException-AquaMenuBarUI-on-Mac-OSX/td-p/34291
+        System.setProperty("com.apple.macos.useScreenMenuBar", "true");
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
         RapidMinerGUI.registerStartupListener(new ToolbarGUIStartupListener());
         RapidMinerGUI.main(args);
     }
